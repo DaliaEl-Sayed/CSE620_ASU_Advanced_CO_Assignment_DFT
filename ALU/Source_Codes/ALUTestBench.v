37fe7bf847ca9e0c43606965533064ee63630083
Your Name <you@example.com>
2023-06-16 04:17:53 +0300

    Done

--- ALU/Source_Codes/ALUTestBench.v
+++ ALU/Source_Codes/ALUTestBench.v
@@ -6,7 +6,7 @@ reg [1:0] Op;
 reg [3:0] Expected_Output;
 reg [0:0] Fault_Indicator;
 reg [15:0] Fault_Counter;
-logic [13:0] Testvectors[0:5];
+logic [13:0] Testvectors[0:99];
 logic [7:0] vectornum;
 reg clk;
 wire [3:0] C;
@@ -36,22 +36,22 @@ initial begin
 end
 
 initial begin 
-	repeat(60) #2 clk = ~clk;
+	repeat(100) #2 clk = ~clk;
 end 
 
 always @(posedge clk) begin
 	/*A = 0;
 	B = 1;
 	Op = Mul;*/
-	{A[3:0], B[3:0], Op[1:0], Expected_Output[3:0] } = Testvectors[vectornum];
+	{A[3:0], Op[1:0], B[3:0],  Expected_Output[3:0] } = Testvectors[vectornum];
 end
 always @(negedge clk)begin
 	if (C==Expected_Output) begin
-		$display ("Test Passed - Expected Result -> %b - And ALU Result is -> %b ", Expected_Output, C);
+		$display ("Test Passed A-> %d OP-> %b B-> %d - Expected Result -> %d - And ALU Result is -> %d ", A, Op, B, Expected_Output, C);
 		Fault_Indicator = 0;
 	end
 	else begin
-		$display ("Test FAILED!! - Expected Result -> %b - But ALU Result is -> %b ", Expected_Output,C);
+		$display ("Test FAILED!! A-> %d OP-> %b B-> %d - Expected Result -> %d - But ALU Result is -> %d ", A, Op, B, Expected_Output,C);
 		Fault_Indicator = 1;
 		Fault_Counter = Fault_Counter + 1;
 	end
