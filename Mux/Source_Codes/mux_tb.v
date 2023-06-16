9475fb0b987b24b00c0fde3a8718203925b17524
Your Name <you@example.com>
2023-06-16 10:47:58 +0300

    Done

--- Mux/Source_Codes/mux_tb.v
+++ Mux/Source_Codes/mux_tb.v
@@ -1,12 +1,12 @@
 module alu_tb;
 
     reg a,b,c,d;
-    reg [1:0] selc;
+    reg [1:0] sel;
     wire out;
     reg Fault_Indicator;
 
 
-    mux DUT(a,b,c,d, selc, out);
+    mux DUT(a,b,c,d, sel, out);
 
     initial begin
         $dumpfile("The_output.vcd");
@@ -26,15 +26,15 @@ module alu_tb;
         b = 1;
         c = 0;
         d = 1;
-        selc = b00;
+        sel = 00;
 
         #5;
         if(out != 0) begin
-            $display("Test FAILED For (%b %b %b %b and selc=%b) (Expected Result: out->0) (But ALU Result is: s->%b)!!  ", a, b, c, d, selc, out);
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->0) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
             Fault_Indicator = 1;
         end
         else begin
-            $display("Test Passed For (%b %b %b %b and selc=%b) (Expected Result: out->0) (And ALU Result is: c->%d) ", a, b, c, d, selc, out);
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->0) (And ALU Result is: c->%d) ", a, b, c, d, sel, out);
             Fault_Indicator = 0;
         end
 
@@ -42,15 +42,15 @@ module alu_tb;
         b = 1;
         c = 0;
         d = 1;
-        selc = b01;
+        sel = 01;
         #5;
 
         if(out != 1) begin
-            $display("Test FAILED For (%b %b %b %b and selc=%b) (Expected Result: out->1) (But ALU Result is: s->%b)!!  ", a, b, c, d, selc, out);
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->1) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
             Fault_Indicator = 1;
         end
         else begin
-            $display("Test Passed For (%b %b %b %b and selc=%b) (Expected Result: out->1) (And ALU Result is: c->%d) ", a, b, c, d, selc, out);
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->1) (And ALU Result is: c->%d) ", a, b, c, d, sel, out);
             Fault_Indicator = 0;
         end
 
@@ -58,32 +58,95 @@ module alu_tb;
         b = 1;
         c = 0;
         d = 1;
-        selc = b10;
+        sel = 10;
         #5;
         if(out != 0) begin
-            $display("Test FAILED For (%b %b %b %b and selc=%b) (Expected Result: out->0) (But ALU Result is: s->%b)!!  ", a, b, c, d, selc, out);
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->0) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
             Fault_Indicator = 1;
         end
         else begin
-            $display("Test Passed For (%b %b %b %b and selc=%b) (Expected Result: out->0) (And ALU Result is: c->%d) ", a, b, c, d, selc, out);
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->0) (And ALU Result is: c->%d) ", a, b, c, d, sel, out);
             Fault_Indicator = 0;
         end
         a = 0;
         b = 1;
         c = 0;
         d = 1;
-        selc = b11;
+        sel = 11;
 
         #5;
-        if(out != 1)
-            if(out != 1) begin
-            $display("Test FAILED For (%b %b %b %b and selc=%b) (Expected Result: out->1) (But ALU Result is: s->%b)!!  ", a, b, c, d, selc, out);
+        if(out != 1) begin
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->1) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
             Fault_Indicator = 1;
         end
         else begin
-            $display("Test Passed For (%b %b %b %b and selc=%b) (Expected Result: out->1) (And ALU Result is: c->%d) ", a, b, c, d, selc, out);
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->1) (And ALU Result is: c->%b) ", a, b, c, d, sel, out);
             Fault_Indicator = 0; 
         end    
+
+        a = 1;
+        b = 0;
+        c = 1;
+        d = 0;
+        sel = 11;
+
+        #5;
+        if(out != 0) begin
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->0) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
+            Fault_Indicator = 1;
+        end
+        else begin
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->0) (And ALU Result is: c->%b) ", a, b, c, d, sel, out);
+            Fault_Indicator = 0; 
+        end 
+         
+        a = 1;
+        b = 0;
+        c = 1;
+        d = 0;
+        sel = 01;
+
+        #5;
+        if(out != 0) begin
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->0) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
+            Fault_Indicator = 1;
+        end
+        else begin
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->0) (And ALU Result is: c->%b) ", a, b, c, d, sel, out);
+            Fault_Indicator = 0; 
+        end   
+
+        a = 1;
+        b = 0;
+        c = 1;
+        d = 0;
+        sel = 10;
+
+        #5;
+        if(out != 1) begin
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->1) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
+            Fault_Indicator = 1;
+        end
+        else begin
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->1) (And ALU Result is: c->%b) ", a, b, c, d, sel, out);
+            Fault_Indicator = 0; 
+        end 
+
+        a = 1;
+        b = 0;
+        c = 1;
+        d = 0;
+        sel = 00;
+
+        #5;
+        if(out != 1) begin
+            $display("Test FAILED For (%b %b %b %b and sel=%b) (Expected Result: out->1) (But ALU Result is: s->%b)!!  ", a, b, c, d, sel, out);
+            Fault_Indicator = 1;
+        end
+        else begin
+            $display("Test Passed For (%b %b %b %b and sel=%b) (Expected Result: out->1) (And ALU Result is: c->%b) ", a, b, c, d, sel, out);
+            Fault_Indicator = 0; 
+        end  
     end
 
 endmodule
